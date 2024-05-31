72e320ab1d9efedeb66ac1247d51a70849d1556e
Rian <142219160+Rianofc@users.noreply.github.com>
2024-05-31 15:07:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function searchTracks() {
 
     $('#waitModal').modal('show');
 
-    fetch(`https://api.onesytex.my.id/api/spotify-search?query=${query}`)
+    fetch(`https://api.balzz.my.id/api/spotify-search?query=${query}`)
         .then(response => response.json())
         .then(data => {
             const result = data.results
@@ -25,7 +25,7 @@ function searchTracks() {
                     <div class="bg-gray-800 rounded-lg overflow-hidden shadow-md">
                         <img src="${track.image}" alt="${track.name}" class="w-full h-40 object-cover rounded-t-lg">
                         <div class="p-4">
-                            <h2 class="text-lg font-semibold text-white">${track.title}</h2>
+                            <h2 class="text-lg font-semibold text-white">${track.duration_ms}</h2>
                             <p class="text-sm text-gray-400">${track.artists}</p>
                             <button onclick="showTrackInfo('${track.link}')" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-2">Play</button>
                         </div>
